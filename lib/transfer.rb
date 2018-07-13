3a17d34f3e7856dc4d45df1aa3b6450613f34d4c
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-13 07:00:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,11 +13,8 @@ class Transfer
   end
 
   def execute_transaction
-    ApplicationRecord.transaction do
-    receiver.update!(money: receiver.money + 50)
-    sender.update!(money: sender.money - 50)
-  end
-  #need successful transaction between two accounts
+
+    #need successful transaction between two accounts
     #can only happen once
     #rejects transfer if sender doesn't have a valid account
 
