f4c1e5ce870c8ac594503eafe53ac020c7bcd1ba
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-13 07:11:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
+      bad_transfer
     end
   end
 
