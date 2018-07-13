cbe091be924ef3d79552526bbd724924b694a918
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-13 07:04:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,18 @@ class Transfer
 
   def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.reject_transfer
+    end
+
+    def reject_transfer
+      self.status = 'rejected'
+    end
+
+
 
     #need successful transaction between two accounts
     #can only happen once
